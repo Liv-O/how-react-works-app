64a5748352f5addd1cabe91e5d371d4a87d9ec59
Olha <olya.nota@gmail.com>
2026-07-30 18:24:55 +0200

    edited activeQuizProps

--- src/components/ActiveQuiz.tsx
+++ src/components/ActiveQuiz.tsx
@@ -3,12 +3,11 @@ import type { Action, Question } from '../../types/types.ts';
 
 interface ActiveQuizProps {
   question: Question;
-  points: number;
   answer: number | null;
   dispatch: React.ActionDispatch<[action: Action]>;
 }
 
-function ActiveQuiz({ question, points, answer, dispatch }: ActiveQuizProps) {
+function ActiveQuiz({ question, answer, dispatch }: ActiveQuizProps) {
   return (
     <div className="active-quiz">
       <h2>{question.question}</h2>
